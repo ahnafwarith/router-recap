63849e85e464a9fc94bbcb0cd80b37d623b80aed
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-04 02:02:39 +0600

    conditional rendering using element variable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const Cart = ({ cart, removeFromCart }) => {
+    // Condional Rendering
+    // 1. Using Element Variable
+    let command;
+    if (cart.length === 0) {
+        command = <p>Please select atleast one item</p>
+    }
+    else if (cart.length === 1) {
+        command = <p>Please select some more?</p>
+    }
+    else {
+        command = <p>Thanks for shopping with us!</p>
+    }
     return (
         <div>
             <h1>Items Selected : {cart.length}</h1>
+            {command}
             {
                 cart.map(tshirt => <p>{tshirt.name}<button onClick={() => removeFromCart(tshirt)}>X</button></p>)
             }
