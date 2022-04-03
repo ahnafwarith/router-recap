8bd87e7ff0447d6f9297206b96f414267545d27a
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-04 03:47:07 +0600

    state to update data across all components

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Aunt from '../Aunt/Aunt';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css'
 
 const Grandpa = () => {
-    const house = 10
+    const [house, setHouse] = useState(1);
+    const increaseHouse = () => {
+        const newHouse = house + 1;
+        setHouse(newHouse)
+    }
     return (
         <div className='grandpa'>
             <h1>Grandpa</h1>
+            <p>{house}</p>
+            <button onClick={increaseHouse}>Buy House</button>
             <div className="kids">
                 <Father house={house}></Father>
                 <Aunt house={house}></Aunt>
                 <Uncle house={house}></Uncle>
             </div>
+
         </div>
     );
 };
