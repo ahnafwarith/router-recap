9eabed06cb4aca1689db2e7bd474febe0ab41d03
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-04 01:10:43 +0600

    sent data to cart

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useTshirts from '../../hooks/useTshirts';
 import Cart from '../Cart/Cart';
 import Tshirt from '../Tshirt/Tshirt';
@@ -6,11 +6,15 @@ import './Home.css'
 
 const Home = () => {
     const [tshirts, handleTshirts] = useTshirts();
+    const [cart, handleCart] = useState([]);
+    const handleAddToCart = (selectedTshirt) => {
+        console.log(selectedTshirt)
+    }
     return (
         <div className='home-container'>
             <div className="products-container">
                 {
-                    tshirts.map(tshirt => <Tshirt key={tshirt._id} tshirt={tshirt}></Tshirt>)
+                    tshirts.map(tshirt => <Tshirt key={tshirt._id} tshirt={tshirt} handleAddToCart={handleAddToCart}></Tshirt>)
                 }
             </div>
             <div className="cart-container">
--- src/components/Tshirt/Tshirt.js
+++ src/components/Tshirt/Tshirt.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Tshirt.css'
-const Tshirt = (props) => {
-    const { name, picture, price } = props.tshirt
+const Tshirt = ({ handleAddToCart, tshirt }) => {
+    const { name, picture, price } = tshirt
     return (
         <div className='tshirt'>
             <img className='img' src={picture} alt="" />
             <h3>{name}</h3>
             <h5>Price:${price}</h5>
-            <button>Add to cart</button>
+            <button onClick={() => handleAddToCart(tshirt)}>Add to cart</button>
         </div>
     );
 };
