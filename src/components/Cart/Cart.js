a3763a7e7912f2ecc04b9e66042aa6ce747df529
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-04 02:18:19 +0600

    condiotional rendering using && and || operators

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Cart = ({ cart, removeFromCart }) => {
-    // Condional Rendering
+    // Conditional Rendering
     // 1. Using Element Variable and simple if-elif
     // 2. Using ternary operator
+    // 3. Using And operator
+    // 4. Using Or operator
     let command;
     if (cart.length === 0) {
         command = <p>Please select atleast one item</p>
@@ -22,6 +24,8 @@ const Cart = ({ cart, removeFromCart }) => {
                 cart.map(tshirt => <p>{tshirt.name}<button onClick={() => removeFromCart(tshirt)}>X</button></p>)
             }
             {cart.length !== 4 ? <p>Keep Adding</p> : <button>Clear All</button>}
+            {cart.length === 5 && <p>5 products</p>}
+            {cart.length !== 6 || <p>6 products</p>}
         </div>
     );
 };
